b2b8d0db5f35e2d87054a0094410a8e7e6d393cf
Chris Duncan <celldee@gmail.com>
2013-01-09 12:32:06 +0000

    Add another example

---
+++ Chapter_2/hello_world_publisher_confirms.rb
@@ -0,0 +1,14 @@
+require 'bunny'
+
+conn = Bunny.new
+conn.start
+
+ch = conn.channel
+ch.confirm_select
+
+x = conn.direct('hello-exchange', :durable => true)
+
+msg = ARGV[0]
+x.publish(msg, :routing_key => 'hola')
+
+ch.wait_for_confirms
