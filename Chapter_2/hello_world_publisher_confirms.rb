808006d7389f94c1317bf20d6027e80cfb015546
Chris Duncan <celldee@gmail.com>
2013-01-13 21:57:23 +0000

    Change to use confirms callback

--- Chapter_2/hello_world_publisher_confirms.rb
+++ Chapter_2/hello_world_publisher_confirms.rb
@@ -1,15 +1,27 @@
 require 'bunny'
 
+def confirms_callback(delivery_tag, multiple, nack)
+  if nack
+    puts 'Message(s) lost!'
+  else
+    puts 'Confirm received!'
+  end
+end
+
 conn = Bunny.new
 conn.start
 
-ch = conn.channel
-ch.confirm_select
+ch = conn.create_channel
 
 x = ch.direct('hello-exchange', :durable => true)
+q = ch.queue('hello-queue', :durable => true).bind(x, :routing_key => 'hola')
 
 msg = ARGV[0]
-x.publish(msg, :routing_key => 'hola')
+
+ch.confirm_select(method(:confirms_callback))
+100.times do |i|
+  x.publish("#{i}: #{msg}", :routing_key => 'hola')
+end
 
 ch.wait_for_confirms
 
